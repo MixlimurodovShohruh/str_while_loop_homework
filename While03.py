d69c9f13c815f7c86b2764324022a3434336aa07
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-13 21:58:32 +0500

    03

--- While03.py
+++ While03.py
@@ -1,3 +1,4 @@
+from string import punctuation
 def main(s):
     """
     A variable of type str is given. Find how many punctuations it contains and return.
@@ -6,4 +7,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    ans=0
+    while i<=len(s)-1:
+        
+        if not s[i].strip(punctuation):
+            ans+=1
+        i+=1
+    return ans
+
+print (main("dnsjfc@@#$%^"))
