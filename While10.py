1d91ffee559b0acdad10fcfeb227637aa42d8b54
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-14 10:06:41 +0500

    10

--- While10.py
+++ While10.py
@@ -9,7 +9,8 @@ def main(s):
     i=0
     ans=0
     while i<=len(s)-1:
-        ans+=int(s[i])
+        if int(s[i])%2!=0:
+         ans+=int(s[i])
         i+=1
     return ans
 print(main('1234567890'))
